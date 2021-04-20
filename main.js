52bd0a5db40b3f3aa490a81adda57c83bc96f6e1
william-rosser-lumen <william.rosser@lumen.com>
2021-04-20 21:45:01 +0000

    Lab 2 Bugfix.

--- main.js
+++ main.js
@@ -69,6 +69,33 @@ function get(serviceNowTable, callback) {
     uri: `/api/now/table/${serviceNowTable}?sysparm_limit=1`,
   };
 
+  // Send Request to ServiceNow.
+  // We are passing variable requestOptions for the first argument.
+  // We are passing an anonymous function, an error-first callback,
+  // for the second argument.
+  request(requestOptions, (error, response, body) => {
+    /**
+     * Process ServiceNow error, response and body.
+     * Check error and response code to make sure
+     * response is good.
+     */
+    if (error) {
+      console.error('Error present.');
+      callbackError = error;
+    } else if (!validResponseRegex.test(response.statusCode)) {
+      console.error('Bad response code.');
+      callbackError = response;
+    } else if (response.body.includes('Instance Hibernating page')) {
+      callbackError = 'Service Now instance is hibernating';
+      console.error(callbackError);
+    } else {
+      callbackData = response;
+    }
+    return callback(callbackData, callbackError);
+  });
+
+}
+
 
 /**
  * @function post
@@ -101,8 +128,6 @@ function post(serviceNowTable, callback) {
     baseUrl: options.url,
     uri: `/api/now/table/${serviceNowTable}`,
   };
-
-
   // Send Request to ServiceNow.
   // We are passing variable requestOptions for the first argument.
   // We are passing an anonymous function, an error-first callback,
